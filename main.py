86f83efe7b6db2f4eb0044a7a6e6b2dd37935a46
technicalrnp <69312728+technicalrnp@users.noreply.github.com>
2025-01-06 23:33:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World245!</p>"
+    return "<p>this is testing of CICD pipeline using webhooks</p>"
 
 app.run(port=5000)
